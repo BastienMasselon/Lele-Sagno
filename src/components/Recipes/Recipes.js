7700294e3607159f0d5703d2343929eb3790ff40
Mudy Mud <bastien.masselon@laposte.net>
2023-08-01 17:22:39 +0200

    dynamisation of recipe list in the Recipes component

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,23 +1,27 @@
 // == Import
+import { useSelector } from 'react-redux';
 import RecipeCard from './RecipeCard/RecipeCard';
+import noImage from "assets/img/empty-image.png";
+
 
 // == Composant
 function Recipes() {
     // const for loop, to simulate array of data that will be retrieved from the API
     const arrayForLoop = [1, 2, 3, 4, 5]
+    const { recipeList } = useSelector((state) => state.data);
 
   return (
     <div className="flex flex-col px-4">
         <h1 className='font-brandon-fat uppercase tracking-wider text-lele-orange text-center text-xl mt-8'>Tu es gourmand(e) comme moi ? Je te partage mes recettes préférées 😋</h1>
       {
-        arrayForLoop.map( (value) => 
+        recipeList.map( (recipe) => 
             <RecipeCard
-                // should use recipe's id as key
-                key={value}
-                thumbnail={'https://image.over-blog.com/Wixf7SBJeEE3O1TrCC40HIQaGCk=/filters:no_upscale()/image%2F0651923%2F20190201%2Fob_45741b_wok-legumes-croquants-nouilles-poul.jpg'}
-                title={"Nouilles sautées au poulet et aux légumes"}
-                preparationTime={"30 minutes"}
-                recipeId={value}
+                key={recipe.id}
+                thumbnail={recipe._embedded && recipe._embedded["wp:featuredmedia"] ? recipe._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url : noImage}
+                title={recipe.title.rendered}
+                preparationTime={recipe.acf.preparationTime ? recipe.acf.preparationTime : null}
+                recipeId={recipe.id}
+                slug={recipe.slug}
             />
         )
       }
